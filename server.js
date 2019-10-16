4d1e412022509d96c7a9fd90fb66f8c02aaf243d
lookwendyican <wendyalers@gmail.com>
2019-10-15 21:45:00 -0400

    added controllers and routes

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+//Dependencies
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+
+// Requiring Comment and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+// Requiring routing controllers
+var htmlRouter = require("./controllers/htmlRoutes.js");
+var articleRouter = require("./controllers/articleRoutes.js");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -9,28 +20,37 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models");
+//var db = require("./models");
 
-var PORT = 3001;
+
 
 // Initialize Express
+var PORT = process.env.PORT ||3001;
 var app = express();
 
 // Configure middleware
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Initialize Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Routing
+app.use("/", htmlRouter);
+app.use("/", articleRouter);
+
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { 
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-});
+// Database configuration with mongoose
+var URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/news-scraper'; 
+mongoose.connect(URI);
+var db = mongoose.connection;
 
 // Routes
 
